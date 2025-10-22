cfdc9d7601ede52fd6665a9cb4626307a36556c6
secad135 <secad135@gmail.com>
2025-10-22 18:48:49 +0330

    added style to index.php

--- index.php
+++ index.php
@@ -36,6 +36,17 @@ if($result === false){
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>home page</title>
+    <style>
+        header{
+            text-align: center;
+        }
+        main{
+            width: 55%;
+            padding: 10px;
+            margin: 0 auto;
+            background-color: beige;
+        }
+    </style>
 </head>
 
 <body>
